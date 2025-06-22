193b6265c0dd977c3f2ab593aaf89f90c236e0f9
Muhammad Hassan <hassanbalouch716@gmail.com>
2025-06-22 16:40:26 +0500

    change style in right div

--- src/components/right.jsx
+++ src/components/right.jsx
@@ -4,21 +4,20 @@ import SocialsLinks from './socialsLink';
 export default function Right() {
     return(
         <>
-            <div className="text-white flex flex-col  space-y-4 px-8 gap-y-24 bg-[#0f1f24] h-full">
-               <div className="flex items-center justify-center flex-col mt-20 space-y-4"> 
-                <h1 className="text-xl font-bold " >Reveloutionz QA with Smarter Automation</h1>
-                <p className="w-80 text-sm">"Soft QA has completely transformed our testing process. it reliable, efficient and ensure our releases are always our top notch"</p>
-                <div className="flex flex-row px-2 py-2">
-                    <div>
-                        <img  className="h-12 w-12 rounded-full object-cover mr-2" src="profile.jpg"></img>
+            <div className="text-white flex flex-col  space-y-8 px-8 gap-y-24 bg-[#0f1f24] h-full">
+               <div className="flex items-center justify-center flex-col mt-20 space-y-12"> 
+                    <h1 className="text-xl font-bold " >Reveloutionz QA with Smarter Automation</h1>
+                    <p className="w-80 text-sm">"Soft QA has completely transformed our testing process. it reliable, efficient and ensure our releases are always our top notch"</p>
+                    <div className="flex flex-row px-2 py-8">
+                        <div>
+                            <img  className="h-12 w-12 rounded-full object-cover mr-2" src="profile.jpg"></img>
+                        </div>
+                        <div>
+                            <h2 className="font-semibold text-base">Hassan</h2>
+                            <p className="text-sm text-gray-200">Web Developer </p>
+                        </div>
                     </div>
-                    <div>
-                        <h2 className="font-semibold text-base">Hassan</h2>
-                        <p className="text-sm text-gray-200">Web Developer </p>
-                    </div>
-                </div>
               </div>
-
               <div className='mt-40'>  
                 <div className="flex items-center justify-center px-4 space-x-4 py-8">
                     <h5 className="text-sm">Join 1k Team</h5>
@@ -33,7 +32,6 @@ export default function Right() {
                     <SocialsLinks Icon={FacebookIcon} />
                 </div>
               </div>  
-                
             </div>
         </>
     )
